ad2d4134b4fc853f36d321b3a712057e14fcef84
Kishore Chandran <kishorechandran66@gmail.com>
2023-04-20 16:00:56 +0530

    Completed the connect button for metamask wallet.

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,8 @@ import React from 'react'
 
 export default function Button(props) {
   return (
+    <div className="btn-inner">
       <button >{props.value}</button>
+    </div>
   )
 }
--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,13 +1,28 @@
-/*import React from "react";
+import React from 'react'
+const ethers = require("ethers") 
+
 
 export default function Connect() {
-  const ethereumButton = document.querySelector(".enableEthereumButton");//Selects button by class name
-  ethereumButton.addEventListener("click", () => {
-    ethereum.request({ method: "eth_requestAccounts" });//when clicking the button 
-  });
+  const isConnected = () =>{ 
+    if (typeof window.ethereum !== 'undefined'){
+      alert("Connect to the wallet!");
+    }
+    else {
+      alert('Please install Metamask to connect to this dapp.');
+    }
+  }
+
+
+  async function connectToMetamask() {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      console.log(`Connected to ${await signer.getAddress()}`);
+
+  }
   return (
-    <div>
-      <button class="enableEthereumButton">Enable Ethereum</button>
-    </div>
-  );
-}*/
+    <button onClick={connectToMetamask} id="wallet-button">Connect</button>
+
+  )
+}
+

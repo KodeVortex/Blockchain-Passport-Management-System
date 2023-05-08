fac1cdaa0ed5ca8d58d1c780185123c919452957
Kishore Chandran <kishorechandran66@gmail.com>
2023-05-08 12:38:11 +0530

    Created the verify passport card

--- contracts/Passport.sol
+++ contracts/Passport.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
+pragma solidity <=0.8.9;
 
-pragma solidity <= 0.8.9;
-
-contract Passport{
+contract Passport {
     address ADMIN = msg.sender;
-    
-    struct Year{
+
+    struct Year {
         uint day;
         uint month;
         uint year;
     }
 
-    struct Person{
+    struct Person {
         string firstName;
         string lastName;
         string gender;
@@ -22,21 +21,43 @@ contract Passport{
         //bool verified;
     }
     //Person[] public persons;
-    mapping (address => Person) public user;
-    function createPassport(string memory _firstName, string memory _lastName, string memory _gender, uint _age, string memory _nationality, uint _byear, uint _bmonth, uint _bday, string memory _country) public {
+    mapping(address => Person) public user;
+
+    function createPassport(
+        string memory _firstName,
+        string memory _lastName,
+        string memory _gender,
+        uint _age,
+        string memory _nationality,
+        uint _byear,
+        uint _bmonth,
+        uint _bday,
+        string memory _country
+    ) public {
         //persons.push(Person(msg.sender,_name,_nationality,_age));
-        
-        user[msg.sender] = Person(_firstName, _lastName, _gender, _age, _nationality, Year(_bday,_bmonth,_byear), _country);
+
+        user[msg.sender] = Person(
+            _firstName,
+            _lastName,
+            _gender,
+            _age,
+            _nationality,
+            Year(_bday, _bmonth, _byear),
+            _country
+        );
     }
 
-    function seePassport() public view returns(Person memory){
+    function viewPassport() public view returns (Person memory) {
         return user[msg.sender];
     }
 
-    function updatePassport(string memory _firstName, string memory _nationality,uint _age) public{
+    function updatePassport(
+        string memory _firstName,
+        string memory _lastName,
+        string memory _gender
+    ) public {
         user[msg.sender].firstName = _firstName;
-        user[msg.sender].nationality = _nationality;
-        user[msg.sender].age = _age;
+        user[msg.sender].lastName = _lastName;
+        user[msg.sender].gender = _gender;
     }
-   
 }
--- src/components/Verify.js
+++ src/components/Verify.js
@@ -4,50 +4,23 @@ LAST MODIFIED BY: @@Kishore
 */
 
 //Create card for giving id for searching a passport.
-import React from 'react'
+import React from 'react';
+import  './Verify.css';
 
 export default function Verify() {
   return (
-    <div>
-          <div class="container" style="margin: 0px;">
-      <form action="" method="post">
-        <section class="vh-40">
-          <div class="container py-2 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-              <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                  <div class="card-body ps-5 pe-5 pt-5 text-center">
-                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                    <p class="text-dark-50 mb-2">Please enter your login and password!</p>
-                    <div class="form-outline mb-3">
-                      <input type="text" name="username" id="typeEmailX-2" class="form-control form-control-lg" />
-                      <label class="form-label" for="typeEmailX-2">Username</label>
-                    </div>
-                    <div class="form-outline mb-3">
-                      <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                      <label class="form-label" for="typePasswordX-2">Password</label>
-                    </div>
-                    <div class="form-check d-flex justify-content-start mb-3">
-                      <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
-                      <label class="form-check-label" for="form1Example3"> Remember password </label>
-                    </div>
-                    <div class="mb-2">
-                      <button class="btn btn-primary btn-lg btn-block mb-4" type="submit">Login</button>
-                    </div>
-                    <div class="mx-md-4 d-flex align-items-center justify-content-center ">
-                      <p class="mb-0 me-2">Don't have an account?</p>
-                      <a href="member_register.php">
-                        <button type="button" class="btn btn-outline-danger">Create new</button>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </form>
+    <div className="verifyCard">
+      <div className="textBox">
+        <p>Name : </p><br/>
+        <p>Gender : </p><br/>
+        <p>Age : </p><br/>
+        <p>Nationality : </p><br/>
+        <p>DOB : </p><br/>
+        <p>Country of Issue : </p>
+
+      </div>
+
     </div>
-    </div>
-  )
+    
+  );
 }

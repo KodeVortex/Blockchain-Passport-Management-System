4d0aa546ca7200b16f50ce136ae25714f372320a
Kishore Chandran <kishorechandran66@gmail.com>
2023-05-08 08:56:04 +0530

    Smart contract added

---
+++ contracts/Passport.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity <= 0.8.9;
+
+contract Passport{
+    address ADMIN = msg.sender;
+    
+    struct Year{
+        uint day;
+        uint month;
+        uint year;
+    }
+
+    struct Person{
+        string firstName;
+        string lastName;
+        string gender;
+        uint age;
+        string nationality;
+        Year dob;
+        string country;
+        //bool verified;
+    }
+    //Person[] public persons;
+    mapping (address => Person) public user;
+    function createPassport(string memory _firstName, string memory _lastName, string memory _gender, uint _age, string memory _nationality, uint _byear, uint _bmonth, uint _bday, string memory _country) public {
+        //persons.push(Person(msg.sender,_name,_nationality,_age));
+        
+        user[msg.sender] = Person(_firstName, _lastName, _gender, _age, _nationality, Year(_bday,_bmonth,_byear), _country);
+    }
+
+    function seePassport() public view returns(Person memory){
+        return user[msg.sender];
+    }
+
+    function updatePassport(string memory _firstName, string memory _nationality,uint _age) public{
+        user[msg.sender].firstName = _firstName;
+        user[msg.sender].nationality = _nationality;
+        user[msg.sender].age = _age;
+    }
+   
+}

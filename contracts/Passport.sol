6c29e62eb819b349397a034e9e5383993ef8bfa0
Kishore Chandran <kishorechandran66@gmail.com>
2023-05-22 16:11:46 +0530

    Deployed and run on the local hard hat network. Working Fine!

--- contracts/Passport.sol
+++ contracts/Passport.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity <=0.8.9;
 
+import "hardhat/console.sol";
 contract Passport {
     address ADMIN = msg.sender;
 
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Passport = await ethers.getContractFactory("Passport");
+    const passport = await Passport.deploy();
+  
+    console.log("Contract deployed address:", passport.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
